c4e0e1271dbaa53d13ed07ea8ad01ccb3128a158
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-02-22 14:48:46 +0700

    Finish

---
+++ exercise-2/exercise2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <div className="block">
+            <p>You can find the React doc at https://react.dev/</p>
+        </div>
+    );
+}
+export default Footer
